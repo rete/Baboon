6909d8c809688368f15a9225d0d52aa6fae9f06c
Ete Remi <remi.ete@gmail.com>
2013-05-21 10:29:03 +0200

    Changed in a simpler processor to demonstrate the use of the clustering algorithm

--- example/src/myProc.cc
+++ example/src/myProc.cc
@@ -69,52 +69,15 @@ myProc::~myProc() {}
 
 void myProc::init() {
 
-	/************************
-	 * SDHCAL configurations
-	 ************************/
-
-	SdhcalConfig *config = SdhcalConfig::GetInstance();
-	cout << "Loading configuration file : " << configFileName << endl;
-	config->LoadFile( configFileName );
-
 	/***********************************************************
 	 * Define all algorithms, add them in the algorithm manager
 	 ***********************************************************/
+	algorithmManager = AlgorithmManager::GetInstance();
 
-	AlgorithmManager *algorithmManager = AlgorithmManager::GetInstance();
-
-	algorithmManager->SetConfigFileName( algorithmFileName );
-
-	// Add the Hough Transform Algorithm for track reconstruction within the sdhcal
-	algorithmManager->RegisterAlgorithm( new HoughTransformAlgorithm() );
-
-	// Add isolation tagging algorithm
-	algorithmManager->RegisterAlgorithm( new IsolationTaggingAlgorithm() );
-
-	// Add clustering (2D) algorithm
+	// Add clustering algorithm
 	algorithmManager->RegisterAlgorithm( new ClusteringAlgorithm() );
 
-	// Add core finder algorithm
-	algorithmManager->RegisterAlgorithm( new CoreFinderAlgorithm() );
-
-	// Add cone beginning algorithm
-	algorithmManager->RegisterAlgorithm( new ConeBeginningAlgorithm() );
-
-	// Add pca algorithm
-//	algorithmManager->RegisterAlgorithm( new PrincipalComponentAnalysis() );
-
-	cout << "Loading configuration file for algorithm : " << algorithmFileName << endl;
-	algorithmManager->Initialize();
-
-	/****************************************************************
-	 * Define the analysis manager and give it the root output file
-	 ****************************************************************/
-
-	AnalysisManager *analysisManager = AnalysisManager::GetInstance();
-	cout << "ROOT output file : " << rootOutputFile << endl;
-	analysisManager->SetRootFileName( rootOutputFile );
-	analysisManager->Init();
-
+	LoadManagers();
 }
 
 void myProc::processRunHeader( LCRunHeader* run ) {
@@ -123,221 +86,69 @@ void myProc::processRunHeader( LCRunHeader* run ) {
 
 void myProc::processEvent( LCEvent * evt ) {
 
-	baboon::CaloHitCreator *caloHitCreator = new baboon::CaloHitCreator();
-	caloHitCreator->SetDecoderString( decoderString );
-	caloHitCreator->SetCollectionName( collectionName );
-	caloHitCreator->CreateCaloHits( evt );
-
-	HitManager *hitManager = HitManager::GetInstance();
-	hitManager->BuildVolumeMap();
-
-	AnalysisManager *analysisManager = AnalysisManager::GetInstance();
+	LoadEvent( evt );
 
 	// just to keep tree compatibility with the old version. Will be removed in the future
 	int evtNb = evt->getEventNumber();
 	cout << "event " << evtNb <<  endl;
-	analysisManager->Set("SplitterVariables","event",evtNb);
-	analysisManager->Set("SplitterVariables","nbOfClusters",0);
-	analysisManager->Set("SplitterVariables", "nbOfTracks" , 0 );
+
+
 
 	vector<int> I;
 	vector<int> J;
 	vector<int> K;
-
-	vector<int> ITrack;
-	vector<int> JTrack;
-	vector<int> KTrack;
-
-	vector<int> IThrust;
-	vector<int> JThrust;
-	vector<int> KThrust;
-
-	vector<int> ICore;
-	vector<int> JCore;
-	vector<int> KCore;
-
-	vector<int> ITrackExtr;
-	vector<int> JTrackExtr;
-	vector<int> KTrackExtr;
-
-	vector<double> ITrackFitFirst;
-	vector<double> JTrackFitFirst;
-	vector<double> KTrackFitFirst;
-
-	vector<double> ITrackFitSecond;
-	vector<double> JTrackFitSecond;
-	vector<double> KTrackFitSecond;
-
-	vector<int> IIsolated;
-	vector<int> JIsolated;
-	vector<int> KIsolated;
-
-	vector<double> Chi2Vec;
-	vector<double> isolationWeights;
-
-	AlgorithmManager *algorithmManager = AlgorithmManager::GetInstance();
-	ClusteringManager *clusteringManager = ClusteringManager::GetInstance();
-	TrackManager *trackManager = TrackManager::GetInstance();
-	CoreManager *coreManager = CoreManager::GetInstance();
-
-	HitCollection * hitCollection = hitManager->GetHitCollection();
-	ClusterCollection *clusterCollection = clusteringManager->GetCluster2D();
-
-//	cout << "nb of clusters : " << clusterCollection->size() << endl;
-//	cout << "nb of hits : " << hitCollection->size() << endl;
+	int nbOfClusters = 0;
+	vector<int> clusterSizes;
 
 
+	HitCollection *hitCollection = hitManager->GetHitCollection();
+	ClusterCollection *clustCol = new ClusterCollection();
 
-	if( algorithmManager->AlgorithmIsRegistered("IsolationTaggingAlgorithm") ) {
-
-		cout << "IsolationTaggingAlgorithm found" << endl;
-		IsolationTaggingAlgorithm* isolationAlgo = (IsolationTaggingAlgorithm *) algorithmManager->GetAlgorithm("IsolationTaggingAlgorithm");
-		isolationAlgo->Process();
-		isolationWeights = isolationAlgo->GetIsolationWeights();
-	}
-
-	if( algorithmManager->AlgorithmIsRegistered("CoreFinderAlgorithm") ) {
-
-		cout << "CoreFinderAlgorithm found" << endl;
-		CoreFinderAlgorithm *coreFinder = (CoreFinderAlgorithm *) algorithmManager->GetAlgorithm("CoreFinderAlgorithm");
-		coreFinder->Process();
-	}
 
 	if( algorithmManager->AlgorithmIsRegistered("ClusteringAlgorithm") ) {
 
 		cout << "ClusteringAlgorithm found" << endl;
-		ClusterCollection *clustCol = new ClusterCollection();
 		ClusteringAlgorithm* clusteringAlgo = (ClusteringAlgorithm *) algorithmManager->GetAlgorithm("ClusteringAlgorithm");
 		clusteringAlgo->SetClusteringMode( fClustering3D );
 		clusteringAlgo->SetTaggingMode( fClusterTagMode );
-		clusteringAlgo->AddHitTagToCluster( fCore );
 		clusteringAlgo->SetClusterCollection( clustCol );
 		clusteringAlgo->Process();
 
-		for( unsigned int i=0 ; i<clustCol->size() ; i++ ) {
-
-			HitCollection *hitCol = clustCol->at(i)->GetHitCollection();
-			Core *core = new Core();
-			for( unsigned int j=0 ; j<hitCol->size() ; j++ ) {
-				core->AddHit( hitCol->at(j) );
-			}
-			Return ret = CoreManager::GetInstance()->AddCore( core );
-			if( !ret.OK )
-				cout << ret.message << endl;
-		}
-		clustCol->clear();
-		delete clustCol;
-	}
-
-	if( algorithmManager->AlgorithmIsRegistered("ConeBeginningAlgorithm") ) {
-
-		cout << "ConeBeginningAlgorithm found" << endl;
-		ConeBeginningAlgorithm *coneBeginning = (ConeBeginningAlgorithm *) algorithmManager->GetAlgorithm("ConeBeginningAlgorithm");
-		coneBeginning->Process();
-	}
-
-	if( algorithmManager->AlgorithmIsRegistered("ClusteringAlgorithm") ) {
-
-		cout << "ClusteringAlgorithm found" << endl;
-		ClusterCollection *clustCol = new ClusterCollection();
-		ClusteringAlgorithm* clusteringAlgo = (ClusteringAlgorithm *) algorithmManager->GetAlgorithm("ClusteringAlgorithm");
-		clusteringAlgo->SetClusteringMode( fClustering2D );
-		clusteringAlgo->SetTaggingMode( fClusterTagMode );
-		clusteringAlgo->SetClusterCollection( clustCol );
-		clusteringAlgo->Process();
 		for( unsigned int i=0 ; i<clustCol->size() ; i++ ) {
 			Return ret = ClusteringManager::GetInstance()->AddCluster( clustCol->at(i) );
 			if( !ret.OK )
 				cout << ret.message << endl;
 		}
-		clustCol->clear();
-		delete clustCol;
 	}
 
-	if( algorithmManager->AlgorithmIsRegistered("HoughTransformAlgorithm") ) {
-
-		cout << "HoughTransformAlgorithm found" << endl;
-		HoughTransformAlgorithm *houghTransform = (HoughTransformAlgorithm*) algorithmManager->GetAlgorithm("HoughTransformAlgorithm");
-		houghTransform->Process();
+	for( unsigned int i=0 ; i<clustCol->size() ; i++ ) {
+		clusterSizes.push_back( clustCol->at(i)->GetClusterSize() );
 	}
+	nbOfClusters = clustCol->size();
+	clustCol->clear();
 
+	delete clustCol;
 
 
 
 	for(unsigned int j=0 ; j<hitCollection->size() ; j++) {
 
 		IntVec ijk = hitCollection->at(j)->GetIJK();
-
-		if( hitCollection->at(j)->GetHitTag() == fTrack ) {
-			ITrack.push_back( ijk.at(0) );
-			JTrack.push_back( ijk.at(1) );
-			KTrack.push_back( ijk.at(2) );
-		}
-		else if (hitCollection->at(j)->GetHitTag() == fTrackExtremity) {
-			ITrackExtr.push_back( ijk.at(0) );
-			JTrackExtr.push_back( ijk.at(1) );
-			KTrackExtr.push_back( ijk.at(2) );
-		}
-		else if( hitCollection->at(j)->GetHitTag() == fCore ) {
-			ICore.push_back( ijk.at(0) );
-			JCore.push_back( ijk.at(1) );
-			KCore.push_back( ijk.at(2) );
-		}
-		else if( hitCollection->at(j)->GetHitTag() == fIsolated ) {
-			IIsolated.push_back( ijk.at(0) );
-			JIsolated.push_back( ijk.at(1) );
-			KIsolated.push_back( ijk.at(2) );
-		}
-		else {
-			I.push_back( ijk.at(0) );
-			J.push_back( ijk.at(1) );
-			K.push_back( ijk.at(2) );
-		}
+		I.push_back( ijk.at(0) );
+		J.push_back( ijk.at(1) );
+		K.push_back( ijk.at(2) );
 	}
 
-	analysisManager->Set("SplitterVariables","Hitx",&I);
-	analysisManager->Set("SplitterVariables","Hity",&J);
-	analysisManager->Set("SplitterVariables","Hitz",&K);
+	analysisManager->Set("myTree","I",&I);
+	analysisManager->Set("myTree","J",&J);
+	analysisManager->Set("myTree","K",&K);
 
-	analysisManager->Set("SplitterVariables","HitxTrack",&ITrack);
-	analysisManager->Set("SplitterVariables","HityTrack",&JTrack);
-	analysisManager->Set("SplitterVariables","HitzTrack",&KTrack);
+	analysisManager->Set("myTree","nbOfClusters",nbOfClusters);
+	analysisManager->Set("myTree","clusterSizes",&clusterSizes);
 
-	analysisManager->Set("SplitterVariables","HitxThrust",&IThrust);
-	analysisManager->Set("SplitterVariables","HityThrust",&JThrust);
-	analysisManager->Set("SplitterVariables","HitzThrust",&KThrust);
+	analysisManager->Fill("myTree");
 
-	analysisManager->Set("SplitterVariables","HitxCore",&ICore);
-	analysisManager->Set("SplitterVariables","HityCore",&JCore);
-	analysisManager->Set("SplitterVariables","HitzCore",&KCore);
-
-	analysisManager->Set("SplitterVariables","HitxTrackExtr",&ITrackExtr);
-	analysisManager->Set("SplitterVariables","HityTrackExtr",&JTrackExtr);
-	analysisManager->Set("SplitterVariables","HitzTrackExtr",&KTrackExtr);
-
-	analysisManager->Set("SplitterVariables","ITrackFitFirst",&ITrackFitFirst);
-	analysisManager->Set("SplitterVariables","JTrackFitFirst",&JTrackFitFirst);
-	analysisManager->Set("SplitterVariables","KTrackFitFirst",&KTrackFitFirst);
-
-	analysisManager->Set("SplitterVariables","ITrackFitSecond",&ITrackFitSecond);
-	analysisManager->Set("SplitterVariables","JTrackFitSecond",&JTrackFitSecond);
-	analysisManager->Set("SplitterVariables","KTrackFitSecond",&KTrackFitSecond);
-
-	analysisManager->Set("SplitterVariables","IIsolated",&IIsolated);
-	analysisManager->Set("SplitterVariables","JIsolated",&JIsolated);
-	analysisManager->Set("SplitterVariables","KIsolated",&KIsolated);
-
-	analysisManager->Set("SplitterVariables","Chi2Vec",&Chi2Vec);
-	analysisManager->Set("SplitterVariables","isolationWeights",&isolationWeights);
-
-	analysisManager->Fill("SplitterVariables");
-
-	clusteringManager->ClearAllContent();
-	hitManager->ClearAllContent();
-	coreManager->ClearAllContent();
-	trackManager->ClearAllContent();
-
-	delete caloHitCreator;
+	ClearAllContent();
 }
 
 
@@ -348,14 +159,62 @@ void myProc::check( LCEvent *evt ) {
 void myProc::end() {
 
 	AnalysisManager::GetInstance()->End();
-
 	AnalysisManager::Kill();
 	AlgorithmManager::Kill();
 	HitManager::Kill();
 	ClusteringManager::Kill();
 	CoreManager::Kill();
+	ShowerManager::Kill();
 	SdhcalConfig::Kill();
+}
+
+
+void myProc::LoadManagers() {
+
+	/************************
+	 * SDHCAL configurations
+	 ************************/
+
+	SdhcalConfig *config = SdhcalConfig::GetInstance();
+	cout << "Loading configuration file : " << configFileName << endl;
+	config->LoadFile( configFileName );
+
+	clusteringManager = ClusteringManager::GetInstance();
+	analysisManager = AnalysisManager::GetInstance();
+	trackManager = TrackManager::GetInstance();
+	coreManager = CoreManager::GetInstance();
+	hitManager = HitManager::GetInstance();
+	showerManager = ShowerManager::GetInstance();
+
+	cout << "ROOT output file : " << rootOutputFile << endl;
+	analysisManager->SetRootFileName( rootOutputFile );
+	analysisManager->Init();
+
+	algorithmManager->SetConfigFileName( algorithmFileName );
+	algorithmManager->Initialize();
 
 }
 
+
+void myProc::LoadEvent( LCEvent *evt ) {
+
+	caloHitCreator = new baboon::CaloHitCreator();
+	caloHitCreator->SetDecoderString( decoderString );
+	caloHitCreator->SetCollectionName( collectionName );
+	caloHitCreator->CreateCaloHits( evt );
+
+	HitManager *hitManager = HitManager::GetInstance();
+	hitManager->BuildVolumeMap();
+}
+
+
+void myProc::ClearAllContent() {
+
+	clusteringManager->ClearAllContent();
+	hitManager->ClearAllContent();
+	coreManager->ClearAllContent();
+	trackManager->ClearAllContent();
+	showerManager->ClearAllContent();
+	delete caloHitCreator;
+}
 
