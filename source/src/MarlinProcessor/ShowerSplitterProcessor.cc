28ae544422aeead291ed821a50ac86c52629cfa3
Ete Remi <remi.ete@gmail.com>
2013-05-30 21:25:20 +0200

    include ConeAlgo + new macros for status code + inerits from BaboonProcessor

--- source/src/MarlinProcessor/ShowerSplitterProcessor.cc
+++ source/src/MarlinProcessor/ShowerSplitterProcessor.cc
@@ -25,66 +25,16 @@ using namespace baboon;
 using namespace EVENT;
 
 ShowerSplitterProcessor::ShowerSplitterProcessor()
-	: marlin::Processor("ShowerSplitterProcessor") {
+	: BaboonProcessor("ShowerSplitterProcessor") {
 
 	  _description = "ShowerSplitterProcessor for shower splitting in SDHCAL";
-
-	  // register steering parameters: name, description, class-variable, default value
-
-	  registerProcessorParameter( "BABOON_HOME" ,
-				      "Path to Baboon directory install",
-				      baboonHome,
-				      string("/home/remi/ilcsoft/SDHCAL/Baboon") );
-
-	  string cfgFileName = string("/home/remi/ilcsoft/SDHCAL/Baboon/config/SDHCAL.cfg");
-	  registerProcessorParameter("SDHCAL_cfg" ,
-				     "SDHCAL configurations" ,
-				     configFileName,
-				     cfgFileName);
-
-	  string algoFileName = string("/home/remi/ilcsoft/SDHCAL/Baboon/config/Algorithm.cfg");
-	  registerProcessorParameter("Algorithm_cfg" ,
-				     "Algorithm configurations" ,
-				     algorithmFileName,
-				     algoFileName);
-
-	  registerProcessorParameter("rootOutputFile" ,
-				     "root outputfile" ,
-				     rootOutputFile,
-				     string("ShowerSplitter.root"));
-
-	  registerProcessorParameter("decoderString" ,
-				     "decoder string for cell ID decoder" ,
-				     decoderString,
-				     string("M:3,S-1:3,I:9,J:9,K-1:6"));
-
-	  registerProcessorParameter("collectionName" ,
-				     "collection name for SDHCAL hits" ,
-				     collectionName,
-				     string("HCALBarrel"));
 
 }
 
 ShowerSplitterProcessor::~ShowerSplitterProcessor() {}
 
 
-void ShowerSplitterProcessor::init() {
-
-	/************************
-	 * SDHCAL configurations
-	 ************************/
-
-	SdhcalConfig *config = SdhcalConfig::GetInstance();
-	cout << "Loading configuration file : " << configFileName << endl;
-	config->LoadFile( configFileName );
-
-	/***********************************************************
-	 * Define all algorithms, add them in the algorithm manager
-	 ***********************************************************/
-
-	AlgorithmManager *algorithmManager = AlgorithmManager::GetInstance();
-
-	algorithmManager->SetConfigFileName( algorithmFileName );
+Return ShowerSplitterProcessor::Init() {
 
 	// Add the Hough Transform Algorithm for track reconstruction within the sdhcal
 	algorithmManager->RegisterAlgorithm( new HoughTransformAlgorithm() );
@@ -101,93 +51,63 @@ void ShowerSplitterProcessor::init() {
 	// Add cone beginning algorithm
 	algorithmManager->RegisterAlgorithm( new ConeBeginningAlgorithm() );
 
-	// Add pca algorithm
-//	algorithmManager->RegisterAlgorithm( new PrincipalComponentAnalysis() );
-
-	cout << "Loading configuration file for algorithm : " << algorithmFileName << endl;
-	algorithmManager->Initialize();
-
-	/****************************************************************
-	 * Define the analysis manager and give it the root output file
-	 ****************************************************************/
-
-	AnalysisManager *analysisManager = AnalysisManager::GetInstance();
-	cout << "ROOT output file : " << rootOutputFile << endl;
-	analysisManager->SetRootFileName( rootOutputFile );
-	analysisManager->Init();
-
+	return BABOON_SUCCESS();
 }
 
-void ShowerSplitterProcessor::processRunHeader( LCRunHeader* run ) {
+Return ShowerSplitterProcessor::ProcessRunHeader( LCRunHeader* run ) {
 
+	return BABOON_SUCCESS();
 }
 
-void ShowerSplitterProcessor::processEvent( LCEvent * evt ) {
+Return ShowerSplitterProcessor::ProcessEvent( const unsigned int &evtNb ) {
 
-	baboon::CaloHitCreator *caloHitCreator = new baboon::CaloHitCreator();
-	caloHitCreator->SetDecoderString( decoderString );
-	caloHitCreator->SetCollectionName( collectionName );
-	caloHitCreator->CreateCaloHits( evt );
 
-	HitManager *hitManager = HitManager::GetInstance();
-	hitManager->BuildVolumeMap();
+	HitCollection *hitCollection = hitManager->GetHitCollection();
 
-	AnalysisManager *analysisManager = AnalysisManager::GetInstance();
+	if( hitCollection->empty() )
+		return BABOON_NOT_INITIALIZED("No hit in the current event. Framework not initialized!");
+
 
 	// just to keep tree compatibility with the old version. Will be removed in the future
-	int evtNb = evt->getEventNumber();
 	cout << "event " << evtNb <<  endl;
-	analysisManager->Set("SplitterVariables","event",evtNb);
+	analysisManager->Set("SplitterVariables","event",int(evtNb));
 	analysisManager->Set("SplitterVariables","nbOfClusters",0);
 	analysisManager->Set("SplitterVariables", "nbOfTracks" , 0 );
 
-	vector<int> I;
-	vector<int> J;
-	vector<int> K;
-
-	vector<int> ITrack;
-	vector<int> JTrack;
-	vector<int> KTrack;
-
-	vector<int> IThrust;
-	vector<int> JThrust;
-	vector<int> KThrust;
-
-	vector<int> ICore;
-	vector<int> JCore;
-	vector<int> KCore;
-
-	vector<int> ITrackExtr;
-	vector<int> JTrackExtr;
-	vector<int> KTrackExtr;
-
-	vector<double> ITrackFitFirst;
-	vector<double> JTrackFitFirst;
-	vector<double> KTrackFitFirst;
-
-	vector<double> ITrackFitSecond;
-	vector<double> JTrackFitSecond;
-	vector<double> KTrackFitSecond;
-
-	vector<int> IIsolated;
-	vector<int> JIsolated;
-	vector<int> KIsolated;
+//	vector<int> I;
+//	vector<int> J;
+//	vector<int> K;
+//
+//	vector<int> ITrack;
+//	vector<int> JTrack;
+//	vector<int> KTrack;
+//
+//	vector<int> IThrust;
+//	vector<int> JThrust;
+//	vector<int> KThrust;
+//
+//	vector<int> ICore;
+//	vector<int> JCore;
+//	vector<int> KCore;
+//
+//	vector<int> ITrackExtr;
+//	vector<int> JTrackExtr;
+//	vector<int> KTrackExtr;
+//
+//	vector<double> ITrackFitFirst;
+//	vector<double> JTrackFitFirst;
+//	vector<double> KTrackFitFirst;
+//
+//	vector<double> ITrackFitSecond;
+//	vector<double> JTrackFitSecond;
+//	vector<double> KTrackFitSecond;
+//
+//	vector<int> IIsolated;
+//	vector<int> JIsolated;
+//	vector<int> KIsolated;
 
 	vector<double> Chi2Vec;
 	vector<double> isolationWeights;
-
-	AlgorithmManager *algorithmManager = AlgorithmManager::GetInstance();
-	ClusteringManager *clusteringManager = ClusteringManager::GetInstance();
-	TrackManager *trackManager = TrackManager::GetInstance();
-	CoreManager *coreManager = CoreManager::GetInstance();
-	ShowerManager *showerManager = ShowerManager::GetInstance();
-
-	HitCollection * hitCollection = hitManager->GetHitCollection();
-	ClusterCollection *clusterCollection = clusteringManager->GetCluster2D();
-
-//	cout << "nb of clusters : " << clusterCollection->size() << endl;
-//	cout << "nb of hits : " << hitCollection->size() << endl;
-
 
 
 	if( algorithmManager->AlgorithmIsRegistered("IsolationTaggingAlgorithm") ) {
@@ -216,6 +136,7 @@ void ShowerSplitterProcessor::processEvent( LCEvent * evt ) {
 		clusteringAlgo->SetClusterSizeLowerLimit( 10 );
 		clusteringAlgo->SetClusterCollection( clustCol );
 		clusteringAlgo->Process();
+
 		for( unsigned int i=0 ; i<clustCol->size() ; i++ ) {
 
 			HitCollection *hitCol = clustCol->at(i)->GetHitCollection();
@@ -223,9 +144,7 @@ void ShowerSplitterProcessor::processEvent( LCEvent * evt ) {
 			for( unsigned int j=0 ; j<hitCol->size() ; j++ ) {
 				core->AddHit( hitCol->at(j) );
 			}
-			Return ret = CoreManager::GetInstance()->AddCore( core );
-			if( !ret.OK )
-				cout << ret.message << endl;
+			BABOON_THROW_RESULT_IF( BABOON_SUCCESS() , != , coreManager->AddCore( core ) );
 		}
 		clustCol->clear();
 		delete clustCol;
@@ -238,6 +157,36 @@ void ShowerSplitterProcessor::processEvent( LCEvent * evt ) {
 		coneBeginning->Process();
 	}
 
+
+	vector<double> xConePos;
+	vector<double> yConePos;
+	vector<double> zConePos;
+	vector<double> coneAngles;
+	vector<double> coneRadius;
+
+	ShowerCollection *showerCollection = showerManager->GetShowerCollection();
+
+	if( !showerCollection->empty() ) {
+		for( unsigned int i=0 ; i<showerCollection->size() ; i++ ) {
+
+			Cone *cone = 0;
+			cone = showerCollection->at(i)->GetStartingCone();
+			if( cone == 0 )
+				continue;
+			ThreeVector conePeakPos = cone->GetPeakPosition();
+			xConePos.push_back( conePeakPos.x() );
+			yConePos.push_back( conePeakPos.y() );
+			zConePos.push_back( conePeakPos.z() );
+			coneAngles.push_back( cone->GetTheta() );
+			coneRadius.push_back( cone->GetRadius() );
+		}
+//		analysisManager->Set("SplitterVariables","xConePos",&xConePos);
+//		analysisManager->Set("SplitterVariables","yConePos",&yConePos);
+//		analysisManager->Set("SplitterVariables","zConePos",&zConePos);
+//		analysisManager->Set("SplitterVariables","coneAngles",&coneAngles);
+//		analysisManager->Set("SplitterVariables","coneRadius",&coneRadius);
+	}
+
 	if( algorithmManager->AlgorithmIsRegistered("ClusteringAlgorithm") ) {
 
 		cout << "ClusteringAlgorithm found" << endl;
@@ -247,11 +196,10 @@ void ShowerSplitterProcessor::processEvent( LCEvent * evt ) {
 		clusteringAlgo->SetTaggingMode( fClusterTagMode );
 		clusteringAlgo->SetClusterCollection( clustCol );
 		clusteringAlgo->Process();
-		for( unsigned int i=0 ; i<clustCol->size() ; i++ ) {
-			Return ret = ClusteringManager::GetInstance()->AddCluster( clustCol->at(i) );
-			if( !ret.OK )
-				cout << ret.message << endl;
-		}
+
+		for( unsigned int i=0 ; i<clustCol->size() ; i++ )
+			BABOON_THROW_RESULT_IF( BABOON_SUCCESS() , != , clusteringManager->AddCluster( clustCol->at(i) ) );
+
 		clustCol->clear();
 		delete clustCol;
 	}
@@ -263,102 +211,105 @@ void ShowerSplitterProcessor::processEvent( LCEvent * evt ) {
 		houghTransform->Process();
 	}
 
+	SimpleEnergyCalculator *calculator = new SimpleEnergyCalculator();
+	calculator->SetHitCollection( hitCollection );
+	calculator->CalculateEnergy();
+	double energy = calculator->GetEnergy();
 
+	cout << "computed energy : " << energy << endl;
 
 
 	for(unsigned int j=0 ; j<hitCollection->size() ; j++) {
 
-		IntVec ijk = hitCollection->at(j)->GetIJK();
+		IntVector ijk = hitCollection->at(j)->GetIJK();
 
 		if( hitCollection->at(j)->GetHitTag() == fTrack ) {
-			ITrack.push_back( ijk.at(0) );
-			JTrack.push_back( ijk.at(1) );
-			KTrack.push_back( ijk.at(2) );
+//			ITrack.push_back( ijk.at(0) );
+//			JTrack.push_back( ijk.at(1) );
+//			KTrack.push_back( ijk.at(2) );
+			if( graphicalEnvironment )
+				calorimeter->GetNodeAt(ijk.at(0),ijk.at(1),ijk.at(2))->GetVolume()->SetLineColor(kRed);
 		}
 		else if (hitCollection->at(j)->GetHitTag() == fTrackExtremity) {
-			ITrackExtr.push_back( ijk.at(0) );
-			JTrackExtr.push_back( ijk.at(1) );
-			KTrackExtr.push_back( ijk.at(2) );
+//			ITrackExtr.push_back( ijk.at(0) );
+//			JTrackExtr.push_back( ijk.at(1) );
+//			KTrackExtr.push_back( ijk.at(2) );
+			if( graphicalEnvironment )
+				calorimeter->GetNodeAt(ijk.at(0),ijk.at(1),ijk.at(2))->GetVolume()->SetLineColor(kGreen);
 		}
 		else if( hitCollection->at(j)->GetHitTag() == fCore ) {
-			ICore.push_back( ijk.at(0) );
-			JCore.push_back( ijk.at(1) );
-			KCore.push_back( ijk.at(2) );
+//			ICore.push_back( ijk.at(0) );
+//			JCore.push_back( ijk.at(1) );
+//			KCore.push_back( ijk.at(2) );
+			if( graphicalEnvironment )
+				calorimeter->GetNodeAt(ijk.at(0),ijk.at(1),ijk.at(2))->GetVolume()->SetLineColor(kMagenta);
 		}
 		else if( hitCollection->at(j)->GetHitTag() == fIsolated ) {
-			IIsolated.push_back( ijk.at(0) );
-			JIsolated.push_back( ijk.at(1) );
-			KIsolated.push_back( ijk.at(2) );
+//			IIsolated.push_back( ijk.at(0) );
+//			JIsolated.push_back( ijk.at(1) );
+//			KIsolated.push_back( ijk.at(2) );
+			if( graphicalEnvironment )
+				calorimeter->GetNodeAt(ijk.at(0),ijk.at(1),ijk.at(2))->GetVolume()->SetLineColor(kBlue);
 		}
 		else {
-			I.push_back( ijk.at(0) );
-			J.push_back( ijk.at(1) );
-			K.push_back( ijk.at(2) );
+//			I.push_back( ijk.at(0) );
+//			J.push_back( ijk.at(1) );
+//			K.push_back( ijk.at(2) );
+			if( graphicalEnvironment )
+				calorimeter->GetNodeAt(ijk.at(0),ijk.at(1),ijk.at(2))->GetVolume()->SetLineColor(kGray+3);
 		}
 	}
 
-	analysisManager->Set("SplitterVariables","Hitx",&I);
-	analysisManager->Set("SplitterVariables","Hity",&J);
-	analysisManager->Set("SplitterVariables","Hitz",&K);
-
-	analysisManager->Set("SplitterVariables","HitxTrack",&ITrack);
-	analysisManager->Set("SplitterVariables","HityTrack",&JTrack);
-	analysisManager->Set("SplitterVariables","HitzTrack",&KTrack);
-
-	analysisManager->Set("SplitterVariables","HitxThrust",&IThrust);
-	analysisManager->Set("SplitterVariables","HityThrust",&JThrust);
-	analysisManager->Set("SplitterVariables","HitzThrust",&KThrust);
-
-	analysisManager->Set("SplitterVariables","HitxCore",&ICore);
-	analysisManager->Set("SplitterVariables","HityCore",&JCore);
-	analysisManager->Set("SplitterVariables","HitzCore",&KCore);
-
-	analysisManager->Set("SplitterVariables","HitxTrackExtr",&ITrackExtr);
-	analysisManager->Set("SplitterVariables","HityTrackExtr",&JTrackExtr);
-	analysisManager->Set("SplitterVariables","HitzTrackExtr",&KTrackExtr);
-
-	analysisManager->Set("SplitterVariables","ITrackFitFirst",&ITrackFitFirst);
-	analysisManager->Set("SplitterVariables","JTrackFitFirst",&JTrackFitFirst);
-	analysisManager->Set("SplitterVariables","KTrackFitFirst",&KTrackFitFirst);
-
-	analysisManager->Set("SplitterVariables","ITrackFitSecond",&ITrackFitSecond);
-	analysisManager->Set("SplitterVariables","JTrackFitSecond",&JTrackFitSecond);
-	analysisManager->Set("SplitterVariables","KTrackFitSecond",&KTrackFitSecond);
-
-	analysisManager->Set("SplitterVariables","IIsolated",&IIsolated);
-	analysisManager->Set("SplitterVariables","JIsolated",&JIsolated);
-	analysisManager->Set("SplitterVariables","KIsolated",&KIsolated);
+//	analysisManager->Set("SplitterVariables","Hitx",&I);
+//	analysisManager->Set("SplitterVariables","Hity",&J);
+//	analysisManager->Set("SplitterVariables","Hitz",&K);
+//
+//	analysisManager->Set("SplitterVariables","HitxTrack",&ITrack);
+//	analysisManager->Set("SplitterVariables","HityTrack",&JTrack);
+//	analysisManager->Set("SplitterVariables","HitzTrack",&KTrack);
+//
+//	analysisManager->Set("SplitterVariables","HitxThrust",&IThrust);
+//	analysisManager->Set("SplitterVariables","HityThrust",&JThrust);
+//	analysisManager->Set("SplitterVariables","HitzThrust",&KThrust);
+//
+//	analysisManager->Set("SplitterVariables","HitxCore",&ICore);
+//	analysisManager->Set("SplitterVariables","HityCore",&JCore);
+//	analysisManager->Set("SplitterVariables","HitzCore",&KCore);
+//
+//	analysisManager->Set("SplitterVariables","HitxTrackExtr",&ITrackExtr);
+//	analysisManager->Set("SplitterVariables","HityTrackExtr",&JTrackExtr);
+//	analysisManager->Set("SplitterVariables","HitzTrackExtr",&KTrackExtr);
+//
+//	analysisManager->Set("SplitterVariables","ITrackFitFirst",&ITrackFitFirst);
+//	analysisManager->Set("SplitterVariables","JTrackFitFirst",&JTrackFitFirst);
+//	analysisManager->Set("SplitterVariables","KTrackFitFirst",&KTrackFitFirst);
+//
+//	analysisManager->Set("SplitterVariables","ITrackFitSecond",&ITrackFitSecond);
+//	analysisManager->Set("SplitterVariables","JTrackFitSecond",&JTrackFitSecond);
+//	analysisManager->Set("SplitterVariables","KTrackFitSecond",&KTrackFitSecond);
+//
+//	analysisManager->Set("SplitterVariables","IIsolated",&IIsolated);
+//	analysisManager->Set("SplitterVariables","JIsolated",&JIsolated);
+//	analysisManager->Set("SplitterVariables","KIsolated",&KIsolated);
 
 	analysisManager->Set("SplitterVariables","Chi2Vec",&Chi2Vec);
 	analysisManager->Set("SplitterVariables","isolationWeights",&isolationWeights);
 
 	analysisManager->Fill("SplitterVariables");
 
-	showerManager->ClearAllContent();
-	clusteringManager->ClearAllContent();
-	hitManager->ClearAllContent();
-	coreManager->ClearAllContent();
-	trackManager->ClearAllContent();
-
-	delete caloHitCreator;
+	return BABOON_SUCCESS();
 }
 
 
-void ShowerSplitterProcessor::check( LCEvent *evt ) {
+Return ShowerSplitterProcessor::Check( LCEvent *evt ) {
 
+	return BABOON_SUCCESS();
 }
 
-void ShowerSplitterProcessor::end() {
+Return ShowerSplitterProcessor::End() {
 
-	AnalysisManager::GetInstance()->End();
-
-	AnalysisManager::Kill();
-	AlgorithmManager::Kill();
-	HitManager::Kill();
-	ClusteringManager::Kill();
-	CoreManager::Kill();
-	SdhcalConfig::Kill();
-
+	return BABOON_SUCCESS();
 }
+
 
 
