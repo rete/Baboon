29a580aa48d067b594f6ba7a0f9e21e427dcb193
Ete Remi <remi.ete@gmail.com>
2013-05-30 21:27:41 +0200

    new macros for status code + shower is now a cores and tracks container

--- source/src/Reconstruction/Objects/Shower.cc
+++ source/src/Reconstruction/Objects/Shower.cc
@@ -23,6 +23,8 @@ namespace baboon {
 		hitToWeightsMap = new HitToWeightsMap;
 		startingCone = 0;
 		thrust = 0;
+		coreCollection = new CoreCollection();
+		trackCollection = new TrackCollection();
 	}
 
 
@@ -30,24 +32,28 @@ namespace baboon {
 
 		hitToWeightsMap->clear();
 		delete hitToWeightsMap;
+		coreCollection->clear();
+		delete coreCollection;
+		trackCollection->clear();
+		delete trackCollection;
 	}
 
 
 	Return Shower::AddHit( Hit *hit , const double &weight ) {
 
 		if( hit == 0 )
-			return S_ERROR("While adding a hit in Shower : assertion hit != 0 failed.");
+			return BABOON_INVALID_PARAMETER("Assertion hit != 0 failed.");
 
 		if( hitToWeightsMap->empty() ) {
 			(*hitToWeightsMap)[ hit ] = weight;
-			return S_OK();
+			return BABOON_SUCCESS();
 		}
 
 		if( hitToWeightsMap->find( hit ) != hitToWeightsMap->end() )
-			return S_OK("Warning : hit is already in the collection");
+			return BABOON_ALREADY_PRESENT("Hit is already in the collection");
 		else {
 			(*hitToWeightsMap)[ hit ] = weight;
-			return S_OK();
+			return BABOON_SUCCESS();
 		}
 	}
 
@@ -62,34 +68,34 @@ namespace baboon {
 	Return Shower::SetHitWeight( Hit *hit , const double &weight ) {
 
 		if( hit == 0 )
-			return S_ERROR("While setting the hit weight in Shower : assertion hit != 0 failed.");
+			return BABOON_INVALID_PARAMETER("Assertion hit != 0 failed.");
 
 		if( hitToWeightsMap->empty() ) {
-			return S_ERROR("Hit map is empty. Can't set a weight");
+			return BABOON_NOT_INITIALIZED("Hit map is empty. Can't set a weight");
 		}
 
 		HitToWeightsMap::iterator it = hitToWeightsMap->find( hit );
 		if( it != hitToWeightsMap->end() )
-			return S_ERROR("Hit is not in shower. Can't set a weight");
+			return BABOON_NOT_FOUND("Hit is not in shower. Can't set a weight");
 		else {
 			it->second = weight;
-			return S_OK();
+			return BABOON_SUCCESS();
 		}
 	}
 
 	Return Shower::RemoveHit( Hit *hit ) {
 
 		if( hitToWeightsMap->empty() )
-			return S_OK("Warning : Try to remove a hit from an empty hit collection.");
+			return BABOON_NOT_FOUND("Try to remove a hit from an empty hit collection.");
 
 		HitToWeightsMap::iterator it = hitToWeightsMap->find( hit );
 
 		if( it != hitToWeightsMap->end() ) {
 			hitToWeightsMap->erase( it );
-			return S_OK();
+			return BABOON_SUCCESS();
 		}
 		else {
-			return S_OK("Hit was not in the collection.");
+			return BABOON_NOT_FOUND("Hit was not in the collection.");
 		}
 
 	}
@@ -103,29 +109,29 @@ namespace baboon {
 	Return Shower::SetStartingPoint( const ThreeVector &startingVec ) {
 
 		startingPoint = startingVec;
-		return S_OK();
+		return BABOON_SUCCESS();
 	}
 
 
 	Return Shower::SetStartingCone( Cone *cone ) {
 
 		if( cone == 0 )
-			return S_ERROR("While setting the cone in Shower : assertion cone != 0 failed.");
+			return BABOON_INVALID_PARAMETER("While setting the cone in Shower : assertion cone != 0 failed.");
 
 		if( startingCone != 0 )
 			delete startingCone;
 
 		startingCone = cone;
-		return S_OK();
+		return BABOON_SUCCESS();
 	}
 
 
 	Return Shower::SetThrust( Track *th ) {
 
 		if( th == 0 )
-			return S_ERROR("While setting the thrust in Shower : assertion thrust != 0 failed.");
+			return BABOON_INVALID_PARAMETER("Assertion thrust != 0 failed.");
 		thrust = th;
-		return S_OK();
+		return BABOON_SUCCESS();
 	}
 
 	bool Shower::HasThrust() {
@@ -133,6 +139,101 @@ namespace baboon {
 		if( thrust == 0 ) return false;
 		return true;
 	}
+
+
+	Return Shower::AddCore( Core *core ) {
+
+		if( core == 0 )
+			return BABOON_INVALID_PARAMETER("Assertion core != 0 failed.");
+
+		if( std::find( coreCollection->begin() , coreCollection->end() , core ) != coreCollection->end() )
+			return BABOON_ALREADY_PRESENT("Core already in the core collection.");
+
+		coreCollection->push_back( core );
+
+		HitCollection *coreHits = core->GetHitCollection();
+		for( unsigned int h=0 ; h<coreHits->size() ; h++ ) {
+			if( !this->Contains( coreHits->at(h) ) )
+				BABOON_THROW_RESULT_IF( BABOON_SUCCESS() , != , this->AddHit( coreHits->at(h) ) );
+		}
+		return BABOON_SUCCESS();
+	}
+
+	Return Shower::RemoveCore( Core *core ) {
+
+		if( core == 0 )
+			return BABOON_INVALID_PARAMETER("Assertion core != 0 failed.");
+
+		CoreCollection::iterator it = std::find( coreCollection->begin() , coreCollection->end() , core );
+
+		if( it != coreCollection->end() ) {
+			coreCollection->erase( it );
+
+			HitCollection *coreHits = core->GetHitCollection();
+			for( unsigned int h=0 ; h<coreHits->size() ; h++ ) {
+				if( this->Contains( coreHits->at(h) ) )
+					BABOON_THROW_RESULT_IF( BABOON_SUCCESS() , != , this->RemoveHit( coreHits->at(h) ) );
+			}
+
+			return BABOON_SUCCESS();
+		}
+
+		return BABOON_ERROR("Core doesn't exists in core collection.");
+	}
+
+	bool Shower::Contains( Core *core ) {
+
+		if( core == 0 ) return false;
+		return std::find( coreCollection->begin() , coreCollection->end() , core ) != coreCollection->end();
+	}
+
+	Return Shower::AddTrack( Track *track ) {
+
+		if( track == 0 )
+			return BABOON_INVALID_PARAMETER("Assertion track != 0 failed.");
+
+		if( std::find( trackCollection->begin() , trackCollection->end() , track ) != trackCollection->end() )
+			return BABOON_ALREADY_PRESENT("Track already in the track collection.");
+
+		trackCollection->push_back( track );
+
+		HitCollection *trackHits = track->GetHitCollection();
+		for( unsigned int t=0 ; t<trackHits->size() ; t++ ) {
+			if( !this->Contains( trackHits->at(t) ) )
+				BABOON_THROW_RESULT_IF( BABOON_SUCCESS() , != , this->AddHit( trackHits->at(t) ) );
+		}
+
+		return BABOON_SUCCESS();
+	}
+
+	Return Shower::RemoveTrack( Track *track ) {
+
+		if( track == 0 )
+			return BABOON_INVALID_PARAMETER("Assertion track != 0 failed.");
+
+		TrackCollection::iterator it = std::find( trackCollection->begin() , trackCollection->end() , track );
+
+		if( it != trackCollection->end() ) {
+			trackCollection->erase( it );
+
+			HitCollection *trackHits = track->GetHitCollection();
+			for( unsigned int t=0 ; t<trackHits->size() ; t++ ) {
+				if( this->Contains( trackHits->at(t) ) )
+					BABOON_THROW_RESULT_IF( BABOON_SUCCESS() , != , this->RemoveHit( trackHits->at(t) ) );
+			}
+
+			return BABOON_SUCCESS();
+		}
+
+		return BABOON_ERROR("Track doesn't exists in track collection.");
+	}
+
+	bool Shower::Contains( Track *track ) {
+
+		if( track == 0 ) return false;
+		return std::find( trackCollection->begin() , trackCollection->end() , track ) != trackCollection->end();
+	}
+
 
 }  // namespace 
 
