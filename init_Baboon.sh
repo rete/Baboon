44d794437c148f6ab44d25fcca41658af5deeb48
Ete Remi <remi.ete@gmail.com>
2013-10-09 15:11:15 +0200

    lib export removed since the package is just a library to load

--- init_Baboon.sh
+++ init_Baboon.sh
@@ -14,10 +14,3 @@ export BABOON_LIB_DIR=$BABOON_HOME"/lib"
 export PATH=$PATH:$BABOON_BIN_DIR
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$BABOON_LIB_DIR
 
-export BABOON_DLL=$BABOON_LIB_DIR"/libShowerSplitter.so"
-export BABOON_DLL=$BABOON_DLL:$BABOON_LIB_DIR"/libOverlayEstimator.so"
-export BABOON_DLL=$BABOON_DLL:$BABOON_LIB_DIR"/libTestBeamCutsProcessor.so"
-export BABOON_DLL=$BABOON_DLL:$BABOON_LIB_DIR"/libShowerPDFProcessor.so"
-export BABOON_DLL=$BABOON_DLL:$BABOON_LIB_DIR"/libEventDisplay.so"
-
-export MARLIN_DLL=$BABOON_DLL
